b06348b4f50aee7f35cef41a73d8d38b2c311aa5
jepsoniada <patryk.pelczar@gmail.com>
2021-01-23 05:14:35 +0100

    core options ui and storage

--- options.js
+++ options.js
@@ -1,7 +1,76 @@
-new Promise(async (res, rej) => {
-    await chrome.storage.sync.get(null, val => res(val))
-}).then( valfromstorage => console.log(valfromstorage))
+let scriptsCache = new Object()
+const focusedScript = {focused: ""}
 
+function removeDisplay() {
+    const value = document.querySelector("#scriptName input").value
+
+    if (Object.keys(scriptsCache).find(e => e == value) == undefined) {
+        document.querySelector("#removeScript").style.display = "none"
+    } else {
+        document.querySelector("#removeScript").style.display = "inline-block"
+        focusedScript.focused = value
+    }
+    // inline-block | none
+}
+
+function scriptHeadReactionForClick(link) {
+    console.log(link, ':', scriptsCache[link], '\n')
+    focusedScript.focused = link
+    document.querySelector("#scriptName input").value = link
+    document.querySelector("#fileinput input[type='file").value = ""
+    document.querySelector("#scriptText").innerText = scriptsCache[link]
+    removeDisplay()
+    updateScriptNameAlert()
+    if (document.querySelector("#scriptText").hasAttribute("noval")) {
+        document.querySelector("#scriptText").removeAttribute('noval')
+    }
+}
+
+async function sideBarFill(change) {
+    console.log("STORAGE_UPDATE::")
+    console.log(change)
+    new Promise(async (res, rej) => {
+        await chrome.storage.sync.get(null, val => res(val))
+    }).then( valfromstorage => {
+        // for (key in valfromstorage) {
+        //     scriptsCache[key] = valfromstorage[key]
+        // }
+        scriptsCache = new Object()
+        for (key in valfromstorage) {
+            if (key.search(/typo/gi)) {
+                scriptsCache[key] = valfromstorage[key]
+            }
+        }
+        // scriptsCache = valfromstorage
+        document.querySelector("#sidebar #scriptList").innerHTML = ''
+        console.log(JSON.stringify(scriptsCache))
+        Object.keys(scriptsCache).forEach(elem => {
+            if (elem.search(/TYPO_PRIVATE/) < 0) {
+                const tempStr = `<div class="scriptHead" onclick="scriptHeadReactionForClick(\'${elem}\')"></div>`
+                console.log(elem)
+                let temp = document.createElement("template")
+                temp.innerHTML = tempStr
+                temp.content.firstChild.innerText = elem
+                document.querySelector("#sidebar #scriptList").appendChild(temp.content.firstChild)
+            }
+        })
+        if (Object.keys(change).length == 1) {
+                focusedScript.focused = ""
+                removeDisplay()
+        }
+    })
+}
+
+// actually can change on update; that can be updated
+chrome.storage.sync.onChanged.addListener(sideBarFill)
+
+document.addEventListener('readystatechange', () => {
+    if (document.readyState == "complete") {
+        chrome.storage.sync.set({"TYPO_PRIVATE dummyinit": Math.random()})
+    }
+})
+
+// file input to text convertion
 document.querySelector("#fileinput input").addEventListener("input", e => {
     let file = e.target.files[0]
     
@@ -13,4 +82,92 @@ document.querySelector("#fileinput input").addEventListener("input", e => {
     //     console.log(event.target.result);
     // })
     // reader.readAsText(file)
+})
+
+function isScriptDataFilled () {
+    if (document.querySelector("#scriptName input").value.length > 0 && (document.querySelector("#fileinput input[type='file").value.length > 0 || document.querySelector("#scriptText").innerText.length > 0)) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function scriptInputClear() {
+    document.querySelector("#scriptName input").value = ""
+    document.querySelector("#fileinput input[type='file").value = ""
+    document.querySelector("#scriptText").innerText = ''
+}
+
+// uploads script to storage
+document.querySelector("#uploadScript").addEventListener("click", async () => {
+    if (isScriptDataFilled()) {
+        const objToSet = new Object({[document.querySelector("#scriptName input").value]: document.querySelector("#scriptText").innerText})
+        // objToSet[document.querySelector("#scriptName input").value] = document.querySelector("#scriptText").innerText
+        await chrome.storage.sync.set(objToSet)
+        scriptInputClear()
+        updateScriptNameAlert()
+        updateScriptFileAlert()
+        removeDisplay()
+    }
+})
+
+document.querySelector("#removeScript").addEventListener("mousedown", () => {
+    console.log(`should remove`, focusedScript.focused)
+    chrome.storage.sync.remove(focusedScript.focused, () => {
+        // focusedScript.focused = ""
+        // removeDisplay()
+    })
+    // chrome.storage.sync.set({"TYPO_PRIVATE dummyinit": Math.random()})
+})
+
+// function updateInputAlerts () {
+//     if (document.querySelector("#scriptName input").value.length > 0) {
+//         document.querySelector("#scriptName").removeAttribute("noval")
+//     } else {
+//         document.querySelector("#scriptName").setAttribute("noval", "")
+//     }
+
+//     // console.log(typeof document.querySelector("#fileinput input[type='file").value)
+//     if (document.querySelector("#fileinput input[type='file").value.length > 0) {
+//         document.querySelector("#scriptText").removeAttribute("noval")
+//     } else {
+//         document.querySelector("#scriptText").setAttribute("noval", "")
+//     }
+// }
+
+function updateScriptNameAlert() {
+    if (document.querySelector("#scriptName input").value.length > 0) {
+        document.querySelector("#scriptName").removeAttribute("noval")
+    } else {
+        document.querySelector("#scriptName").setAttribute("noval", "")
+    }
+}
+
+function updateScriptFileAlert() {
+    if (document.querySelector("#fileinput input[type='file").value.length > 0) {
+        document.querySelector("#scriptText").removeAttribute("noval")
+    } else {
+        document.querySelector("#scriptText").setAttribute("noval", "")
+    }
+}
+
+// empty input info
+document.querySelector("#scriptName input").addEventListener("input", () => {
+    updateScriptNameAlert()
+    removeDisplay()
+})
+document.querySelector("#fileinput input[type='file").addEventListener('input', () => {
+    updateScriptFileAlert()
+})
+
+// document.addEventListener("input", () => {
+//     updateScriptNameAlert()
+//     updateScriptFileAlert()
+// })
+
+// "showing" add script page
+document.querySelector("#addScreenBtn").addEventListener("click", () => {
+    scriptInputClear()
+    updateScriptNameAlert()
+    updateScriptFileAlert()
 })
